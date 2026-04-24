8427768e1de3c67b840717c43da8ac4c650e0a35
leonardo honda <leozinhohondams@gmail.com>
2026-04-24 18:39:47 -0400

    feat: register Balance and TimeOffRequest entities in TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Balance } from './balance/balance.entity';
+import { TimeOffRequest } from './time-off/time-off.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'better-sqlite3',
       database: 'database.sqlite',
-      entities: [],
+      entities: [Balance, TimeOffRequest],
       synchronize: true,
     }),
   ],
