ee931a90d76b28c24f8d228b6077c4149acfb375
leonardo honda <leozinhohondams@gmail.com>
2026-04-24 22:05:11 -0400

    feat(app): wire BalanceModule and TimeOffModule, enable global ValidationPipe

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,9 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BalanceModule } from './balance/balance.module';
 import { Balance } from './balance/balance.entity';
+import { TimeOffModule } from './time-off/time-off.module';
 import { TimeOffRequest } from './time-off/time-off.entity';
 
 @Module({
@@ -13,6 +15,8 @@ import { TimeOffRequest } from './time-off/time-off.entity';
       entities: [Balance, TimeOffRequest],
       synchronize: true,
     }),
+    BalanceModule,
+    TimeOffModule,
   ],
   controllers: [AppController],
   providers: [AppService],
