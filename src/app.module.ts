0b84516439e123e3dfcbd46562c6d99c464b9bf4
Leonardo Honda <leozinhohondams@gmail.com>
2026-04-25 11:33:13 -0400

    chore: add @nestjs/schedule and register ScheduleModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -9,6 +10,7 @@ import { TimeOffRequest } from './time-off/time-off.entity';
 
 @Module({
   imports: [
+    ScheduleModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'better-sqlite3',
       database: 'database.sqlite',
